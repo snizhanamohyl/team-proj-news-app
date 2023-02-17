cf2e483c39de1bad3d34810ba99fc0451cbaf5da
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-17 22:27:17 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -13,13 +13,14 @@ export default class SearchNews {
   constructor() {
     this.queryPage = 1;
     this.searchQuery = '';
+    this.category = '';
   }
 
   async searchNews() {
     const API_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
     const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
     const response = await axios.get(
-      `${API_URL}?q=${this.searchQuery}&key=${API_KEY}&page=${this.queryPage}`
+      `${API_URL}?q=${this.searchQuery}&api-key=${API_KEY}&page=${this.queryPage}`
     );
     // then(data => {
     this.incrementPage();
@@ -30,7 +31,7 @@ export default class SearchNews {
     const API_URL =
       'https://api.nytimes.com/svc//news/v3/content/section-list.json';
     const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
-    const response = await axios.get(`${API_URL}?key=${API_KEY}`);
+    const response = await axios.get(`${API_URL}?api-key=${API_KEY}`);
     // then(data => {
     this.incrementPage();
     return response;
@@ -40,7 +41,7 @@ export default class SearchNews {
     const API_URL = 'https://api.nytimes.com/svc/news/v3/content/all/';
     const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
     const response = await axios.get(
-      `${API_URL}?q=${this.searchQuery}&key=${API_KEY}`
+      `${API_URL}${this.category}.json?api-key=${API_KEY}`
     );
     // then(data => {
     this.incrementPage();
@@ -51,7 +52,7 @@ export default class SearchNews {
     const API_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json`;
     const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
     const response = await axios.get(
-      `${API_URL}?key=${API_KEY}&page=${this.queryPage}`
+      `${API_URL}?api-key=${API_KEY}&page=${this.queryPage}`
     );
     // then(data => {
     this.incrementPage();
