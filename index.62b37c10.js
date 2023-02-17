54f6c858aa3250f8e2ef2390c0b9ca405c5c1f14
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-02-17 21:56:12 +0000

    Deploying to gh-pages from @ snizhanamohyl/team-proj-news-app@280b78832406fbd8eaf300621f13b40cb3376018 🚀

---
+++ index.62b37c10.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("others"),t=document.getElementById("dropdown");e.addEventListener("click",(function(){t.classList.toggle("js-is-hidden")}));
+//# sourceMappingURL=index.62b37c10.js.map
