c38c1b4c5e876a31799d9b97f910b6d5cfbf5666
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-17 22:09:35 +0200

    axios

---
+++ src/api/api.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+// 1) список категорій -
+//https://api.nytimes.com/svc//news/v3/content/section-list.json?api-key=eQ8t8FWqeAGnKDTtIFrHmgZCflFrUTcV
+// 2) список новин по категоріям
+//https://api.nytimes.com/svc/news/v3/content/{source}/{section}.json
+//    https://api.nytimes.com/svc/news/v3/content/all/business.json?api-key=
+//    https://api.nytimes.com/svc/news/v3/content/all/crosswords%20%26%20games.json?api-key=
+//    const encoded = encodeURIComponent('crosswords & games'); //crosswords%20&%20games
+// 3) список новин за пошуковим значенням
+//https://api.nytimes.com/svc/search/v2/articlesearch.json?q=ukraine&api-key=eQ8t8FWqeAGnKDTtIFrHmgZCflFrUTcV
+// 4) список популярних новин https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=
+export default class SearchNews {
+  constructor() {
+    this.queryPage = 1;
+    this.searchQuery = '';
+  }
+
+  async searchNews() {
+    const API_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+    const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
+    const response = await axios.get(
+      `${API_URL}?q=${this.searchQuery}&key=${API_KEY}&page=${this.queryPage}`
+    );
+    // then(data => {
+    this.incrementPage();
+    return response;
+  }
+
+  async categoryList() {
+    const API_URL =
+      'https://api.nytimes.com/svc//news/v3/content/section-list.json';
+    const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
+    const response = await axios.get(`${API_URL}?key=${API_KEY}`);
+    // then(data => {
+    this.incrementPage();
+    return response;
+  }
+
+  async categoryNews() {
+    const API_URL = 'https://api.nytimes.com/svc/news/v3/content/all/';
+    const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
+    const response = await axios.get(
+      `${API_URL}?q=${this.searchQuery}&key=${API_KEY}`
+    );
+    // then(data => {
+    this.incrementPage();
+    return response;
+  }
+
+  async mostPopularNews() {
+    const API_URL = `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json`;
+    const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
+    const response = await axios.get(
+      `${API_URL}?key=${API_KEY}&page=${this.queryPage}`
+    );
+    // then(data => {
+    this.incrementPage();
+    return response;
+  }
+  //   resetPage() {
+  //     this.queryPage = 1;
+  //   }
+
+  //   incrementPage() {
+  //     this.queryPage += 1;
+  //   }
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
