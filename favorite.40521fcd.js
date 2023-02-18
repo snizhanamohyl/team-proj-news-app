30caf1885e6040391321626cc61ab5bae8e49577
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-02-18 20:59:50 +0000

    Deploying to gh-pages from @ snizhanamohyl/team-proj-news-app@4579b41808fd59cf7cc4fa3158bacf6ecdebaa5b 🚀

---
+++ favorite.40521fcd.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-menu-container"),n=document.querySelector(".js-open-menu"),o=document.querySelector(".js-close-menu");function t(){e.classList.toggle("is-open"),e.classList.contains("is-open")?document.body.style.position="fixed":document.body.style.position=""}n.addEventListener("click",t),o.addEventListener("click",t)}();
+//# sourceMappingURL=favorite.40521fcd.js.map
---
+++ favorite.afccc3b1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-menu-container"),n=document.querySelector(".js-open-menu"),o=document.querySelector(".js-close-menu");function t(){e.classList.toggle("is-open"),e.classList.contains("is-open")?document.body.style.position="fixed":document.body.style.position=""}n.addEventListener("click",t),o.addEventListener("click",t);
+//# sourceMappingURL=favorite.afccc3b1.js.map
