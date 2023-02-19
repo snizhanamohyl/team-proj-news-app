413c81203e747137c1c2539fd2d206a5bcaf168a
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-19 23:09:16 +0200

    Create data.js

---
+++ src/js/data.js
@@ -0,0 +1,25 @@
+import SearchNews from './api';
+const searchNews = new SearchNews();
+
+async function findNews() {
+  let newSearch;
+  let date = JSON.parse(localStorage.getItem('date'));
+  dateFilter = date;
+  if (dateFilter === '') {
+    newSearch = await searchNews.searchNews();
+    if (newSearch.data.response.docs.length === 0) {
+      throw new Error('no results');
+    }
+    let markup = createMarkup(newSearch.data.response.docs);
+
+    gallery.innerHTML = markup;
+  } else {
+    newSearch = await searchNews.searchNewsWithData();
+    if (newSearch.data.response.docs.length === 0) {
+      throw new Error('no results');
+    }
+    let markup = createMarkup(newSearch.data.response.docs);
+
+    gallery.innerHTML = markup;
+  }
+}
