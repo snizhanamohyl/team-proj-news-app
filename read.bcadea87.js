8f0e04e96953861aee9dd588a318b717d79cff97
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-02-19 19:05:23 +0000

    Deploying to gh-pages from @ snizhanamohyl/team-proj-news-app@b269cf3d6614f49a6bbf3c4a3d2f966afbbb1cdf 🚀

---
+++ read.bcadea87.js
@@ -0,0 +1,2 @@
+!function(){var n=document.location.pathname,a=document.querySelectorAll(".navbar__link");a.forEach((function(r){r.pathname===n&&(a[0].classList.remove("navbar__link--current"),r.classList.add("navbar__link--current"))}))}();
+//# sourceMappingURL=read.bcadea87.js.map
