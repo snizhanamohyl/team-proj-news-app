097103b9db1a2c64b5755b3c9d2befc0303609df
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-02-19 19:12:05 +0000

    Deploying to gh-pages from @ snizhanamohyl/team-proj-news-app@0bec99a888d9afc871c3667b4b901fc7a4d591d8 🚀

--- index.1eb9bad2.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("others"),t=document.getElementById("dropdown");e.addEventListener("click",(function(){t.classList.toggle("js-is-hidden")}))}();
-//# sourceMappingURL=index.1eb9bad2.js.map
--- index.62b37c10.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("others"),t=document.getElementById("dropdown");e.addEventListener("click",(function(){t.classList.toggle("js-is-hidden")}));
-//# sourceMappingURL=index.62b37c10.js.map
