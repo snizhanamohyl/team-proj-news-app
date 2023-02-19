f47407fdc6666deaaa99f01d0e4dad6a1c46d9f6
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-19 21:48:41 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -22,7 +22,7 @@ export default class SearchNews {
     const API_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
     const API_KEY = '8n5KUMyFUl7iOAB9Zwf8IWBubkkgaMEq';
     const response = await axios.get(
-      `${API_URL}?q=${this.searchQuery}&api-key=${API_KEY}`
+      `${API_URL}?q=${this.searchQuery}&api-key=${API_KEY}}&${this.dataFilter}`
     );
 
     return response;
