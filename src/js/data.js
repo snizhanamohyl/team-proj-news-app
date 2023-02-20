176146405f4637c5321322e5b700a08cf8da499d
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-20 16:53:09 +0200

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -5,8 +5,8 @@ async function findNews() {
   let gallery = document.querySelector('.card-set');
   let newSearch;
   const date = JSON.parse(localStorage.getItem('date'));
-  dateFilter = date;
-  if (dateFilter === '') {
+  searchNews.dateFilter = date;
+  if (searchNews.dateFilter === '') {
     newSearch = await searchNews.searchNews();
     if (newSearch.data.response.docs.length === 0) {
       throw new Error('no results');
@@ -22,5 +22,41 @@ async function findNews() {
     let markup = createMarkup(newSearch.data.response.docs);
 
     gallery.innerHTML = markup;
+  }
+}
+
+async function renderNews() {
+  imageNoResults.style.display = 'none';
+  try {
+    const date = JSON.parse(localStorage.getItem('date'));
+    api.dateFilter = date;
+    let categorySearch;
+    if (api.dateFilter === '') {
+      categorySearch = await api.categoryNews();
+
+      if (categorySearch.data.results === null) {
+        throw new Error('no results');
+      }
+
+      const markup = createCategoriesMarkup(categorySearch.data.results);
+
+      gallery.innerHTML = '';
+      gallery.innerHTML = markup;
+    } else {
+      categorySearch = await api.categoryNewsWithDate();
+
+      if (categorySearch.data.results === null) {
+        throw new Error('no results');
+      }
+
+      const markup = createCategoriesMarkup(categorySearch.data.results);
+
+      gallery.innerHTML = '';
+      gallery.innerHTML = markup;
+    }
+  } catch (error) {
+    console.log('ERROR', error);
+    gallery.innerHTML = '';
+    imageNoResults.style.display = 'block';
   }
 }
