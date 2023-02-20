4241cae5790f0d0d03cbf9920eb65ff0014960b8
Nataliia Semenova <natasemenova1806@gmail.com>
2023-02-20 19:15:21 +0200

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,62 +1,69 @@
-import SearchNews from './api';
-const searchNews = new SearchNews();
+// import SearchNews from './api';
+// const searchNews = new SearchNews();
 
-async function findNews() {
-  let gallery = document.querySelector('.card-set');
-  let newSearch;
-  const date = JSON.parse(localStorage.getItem('date'));
-  searchNews.dateFilter = date;
-  if (searchNews.dateFilter === '') {
-    newSearch = await searchNews.searchNews();
-    if (newSearch.data.response.docs.length === 0) {
-      throw new Error('no results');
-    }
-    let markup = createMarkup(newSearch.data.response.docs);
+// async function findNews() {
+//   let gallery = document.querySelector('.card-set');
+//   let newSearch;
+//   const date = JSON.parse(localStorage.getItem('date'));
+//   searchNews.dateFilter = date;
+//   if (searchNews.dateFilter === '') {
+//     newSearch = await searchNews.searchNews();
+//     if (newSearch.data.response.docs.length === 0) {
+//       throw new Error('no results');
+//     }
+//     let markup = createMarkup(newSearch.data.response.docs);
 
-    gallery.innerHTML = markup;
-  } else {
-    newSearch = await searchNews.searchNewsWithDate();
-    if (newSearch.data.response.docs.length === 0) {
-      throw new Error('no results');
-    }
-    let markup = createMarkup(newSearch.data.response.docs);
+//     gallery.innerHTML = markup;
+//   } else {
+//     newSearch = await searchNews.searchNewsWithDate();
+//     if (newSearch.data.response.docs.length === 0) {
+//       throw new Error('no results');
+//     }
+//     let markup = createMarkup(newSearch.data.response.docs);
 
-    gallery.innerHTML = markup;
-  }
-}
+//     gallery.innerHTML = markup;
+//   }
+// }
 
-async function renderNews() {
-  imageNoResults.style.display = 'none';
-  try {
-    const date = JSON.parse(localStorage.getItem('date'));
-    api.dateFilter = date;
-    let categorySearch;
-    if (api.dateFilter === '') {
-      categorySearch = await api.categoryNews();
+// async function renderNews() {
+//   imageNoResults.style.display = 'none';
+//   try {
+//     const date = JSON.parse(localStorage.getItem('date'));
+//     api.dateFilter = date;
+//     let categorySearch;
+//     if (api.dateFilter === '') {
+//       categorySearch = await api.categoryNews();
 
-      if (categorySearch.data.results === null) {
-        throw new Error('no results');
-      }
+//       if (categorySearch.data.results === null) {
+//         throw new Error('no results');
+//       }
 
-      const markup = createCategoriesMarkup(categorySearch.data.results);
+//       const markup = createCategoriesMarkup(categorySearch.data.results);
 
-      gallery.innerHTML = '';
-      gallery.innerHTML = markup;
-    } else {
-      categorySearch = await api.categoryNewsWithDate();
+//       gallery.innerHTML = '';
+//       gallery.innerHTML = markup;
+//     } else {
+//       categorySearch = await api.categoryNewsWithDate();
 
-      if (categorySearch.data.results === null) {
-        throw new Error('no results');
-      }
+//       if (categorySearch.data.results === null) {
+//         throw new Error('no results');
+//       }
 
-      const markup = createCategoriesMarkup(categorySearch.data.results);
+//       const markup = createCategoriesMarkup(categorySearch.data.results);
 
-      gallery.innerHTML = '';
-      gallery.innerHTML = markup;
-    }
-  } catch (error) {
-    console.log('ERROR', error);
-    gallery.innerHTML = '';
-    imageNoResults.style.display = 'block';
-  }
+//       gallery.innerHTML = '';
+//       gallery.innerHTML = markup;
+//     }
+//   } catch (error) {
+//     console.log('ERROR', error);
+//     gallery.innerHTML = '';
+//     imageNoResults.style.display = 'block';
+//   }
+// }
+
+const dateFilter = JSON.parse(localStorage.getItem('date'));
+const publishedDate = document.querySelector('.news-card__date');
+
+if (dateFilter === publishedDate.textContent) {
+  // малюємо розмітку, як ні то innerHTML
 }
