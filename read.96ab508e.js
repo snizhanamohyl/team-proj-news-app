082f834092f23d14b62e0afe40e1a7962b80ebe3
snizhanamohyl <110724174+snizhanamohyl@users.noreply.github.com>
2023-02-22 19:38:37 +0000

    Deploying to gh-pages from @ snizhanamohyl/team-proj-news-app@39572c99f7b8a1495b84ef79b26bf3923275550c 🚀

---
+++ read.96ab508e.js
@@ -0,0 +1,2 @@
+document.querySelector(".date-list").addEventListener("click",(function(t){if(t.target.classList.contains("date-list__btn")){const e=t.target.nextElementSibling;e.classList.contains("card-set")&&e.classList.toggle("visually-hidden");t.target.querySelector(".date-list__btn-svg").classList.toggle("rotate")}}));
+//# sourceMappingURL=read.96ab508e.js.map
--- read.afdfc13a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".date-list").addEventListener("click",(function(t){if(t.target.classList.contains("date-list__btn")){var e=t.target.nextElementSibling;e.classList.contains("card-set")&&e.classList.toggle("visually-hidden")}}));
-//# sourceMappingURL=read.afdfc13a.js.map
